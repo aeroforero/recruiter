8169f6dfd10fc2f1570b664916dbdc4a1a24e1bc
aeroforero <aeroforero@users.noreply.github.com>
2017-06-11 01:00:15 -0500

    updated the new contact form

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,8 +4,8 @@ belongs_to :connection, :foreign_key => "connections_id"
 
 # validations
 validates :location, :presence => { :message => "Please add location (method) of conversation" }
-validates :date, :length => { :minimum => 1, :maximum => 160 }
+validates :date, :length => { :minimum => 1, :maximum => 15 }
 validates :date, :presence => true
-validates :date, :uniqueness => { :scope => [:connections_id], :message => "You already have a conversation on this date, would you like to add more?" }
+validates :date, :uniqueness => { :scope => [:connections_id], :message => "You already have a conversation on this date, would you like to add more to exhisting record?" }
 
 end
